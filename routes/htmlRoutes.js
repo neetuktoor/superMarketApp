8e56989559e699492abe987d930bd2dbefcc16ff
hfrodriguez928 <hayleyrodriguez928@gmail.com>
2017-08-17 16:00:29 -0500

    html route update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -26,8 +26,8 @@ module.exports = function(app){
    app.get("/savedRecipes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/savedRecipes.html"));
   });
-  // app.use(function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../index.html"));
-  // });
-
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
+  });
+ 
 };
