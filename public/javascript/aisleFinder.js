9395e2baccf506ff3a00ea1ea240907f87c042ce
neetuktoor <neetuktoor@gmail.com>
2017-08-16 19:25:22 -0500

    html for ash

--- public/javascript/aisleFinder.js
+++ public/javascript/aisleFinder.js
@@ -1,4 +1,14 @@
-// info from xml2js package
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Testing API</title>
+</head>
+<body>
+	
+<script>
+
+// info from xml2js npm package
 
 var parseString = require('xml2js').parseString;
 var http = require('http');
@@ -29,7 +39,7 @@ var authKey = "7c3de4dc65"
 // needed variables for search
 var itemName;
 var storesByCityState;
-var storesByZip;
+var zipCode;
 var storeName;
 
 var APIKEY = "http://www.SupermarketAPI.com/api.asmx/SearchByProductName?APIKEY=APIKEY&ItemName=?";
@@ -44,11 +54,38 @@ xmlToJson(url, function(err, data) {
   console.log(JSON.stringify(data, null, 2));
 });
 
-AJAX call for Z
-// AJAX call for item name
+// AJAX call for zipcode
  $.ajax({
-      url: "http://www.SupermarketAPI.com/api.asmx/SearchForItem?APIKEY=APIKEY&StoreId=123456&ItemName=Apple",
+      url: "http://www.SupermarketAPI.com/api.asmx/StoresByZip?APIKEY=APIKEY&ZipCode=?";
       method: "GET"
     }).done(function(response) {
       console.log(response);
     });
+
+//AJAX call for stores by City & State
+ $.ajax({
+      url: "http://www.SupermarketAPI.com/api.asmx/StoresByCityState?APIKEY=APIKEY&SelectedCity=?&SelectedState=?";
+      method: "GET"
+    }).done(function(response) {
+      console.log(response);
+    });
+
+//AJAX call for store name
+$.ajax({
+      url: "http://www.SupermarketAPI.com/api.asmx/ReturnStoresByName?APIKEY=APIKEY&StoreName=?";
+      method: "GET"
+    }).done(function(response) {
+      console.log(response);
+    });
+
+// AJAX call for item name
+ $.ajax({
+      url: "http://www.SupermarketAPI.com/api.asmx/SearchForItem?APIKEY=APIKEY&StoreId=123456&ItemName=?";
+      method: "GET"
+    }).done(function(response) {
+      console.log(response);
+    });
+</script>
+
+</body>
+</html>
