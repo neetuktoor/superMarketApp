44abc5289df060dd67234e28cab2f1d99f9f0edf
neetuktoor <neetuktoor@gmail.com>
2017-08-21 10:31:35 -0500

    still no list in db

--- public/javascript/currentlist.js
+++ public/javascript/currentlist.js
@@ -1,26 +1,58 @@
-$(document).ready(function() {
+// don's code
+// $(document).ready(function() {
+// console.log("this is working");
+//   var name = $("#name");
+//   var $quantity = $("#quantity");
+//   var $notes = $("#note");
+//   var $category = $("#category");
 
-  var name = $("#name");
-  var $quantity = $("#quantity");
-  var $notes = $("#note");
-  var $category = $("#category");
+//   //click event
+// $("#addToList").on("click", function() {
+//   var list = {
+//     name: $name.val(),
+//     quantity: $quantity.val(),
+//     notes: $notes.val(),
+//     category: category.val(),
+//   };
 
-  //click event
-$(".modal-action").click(function() {
-  console.log("item added");
-
-  var list = {
-    name: $name.val(),
-    quantity: $quantity.val(),
-    notes: $notes.val(),
-    category: category.val(),
-  };
-
-    $.ajax({
-      type: POST,
-      url: '/api/currentlist',
-      data: list,
+//     $.ajax({
+//       type: POST,
+//       url: '/api/currentlist',
+//       data: list,
       
+//     });
+//   });
+// });
+
+//neetu's attempt
+//when a user click the add to list button
+$("addToList").on("click", function (event) {
+  event.preventDefault();
+    //make a new list obj
+    var list = {
+      //item name from input
+      item: $("#item").val().trim(),
+      //category from input
+      category: $("#category").val().trim(),
+      //amount from input
+      amount: $("#amount").val().trim(),
+      //notes from input
+      description: $("#description").val().trim()
+};
+
+// send an AJAX POST-request with jQuery
+$.post("/api/currentlist", list)
+      // on success, run this callback
+    .done(function(data) {
+      // log the data we found
+      console.log(data);
+      // tell the user we're adding a list item with an alert window
+      alert("Adding list item...");
     });
-  });
+
+// empty each input box by replacing the value with an empty string
+$("#item").val("");
+$("#category").val("");
+$("#amount").val("");
+$("#description").val("");
 });
