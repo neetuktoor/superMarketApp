c78a6debe0dfddf28ed0090aed2579e2c9655f6d
Ashley  Diep <ash.d.321@gmail.com>
2017-08-21 10:18:34 -0500

    fixed some recipe bugs

--- public/javascript/savedrecipes.js
+++ public/javascript/savedrecipes.js
@@ -1,84 +1,108 @@
 //when document on ready
 $(document).ready(function() {
-  console.log("i'm in");
-  //get saved recipes from database
-  $.get("/api/recipes", function(data){
 
-  console.log("saved recipes: " + data);
-  //for each of saved recipe
-  for (var i = 0; i < data.length; i++){
 
-    //find the name, website url, picture
-    var name = data[i].recipename;
-    var website = data[i].website;
-    var picture = data[i].picture;
+  var recipecontain;
+  var hello;
+  var count = 0;
 
-    //make a container
-    var recipecontain = $("<div class = 'recipecontain'></div>");
 
-    //append name with website url, picture
-    var foodpic = $("<img src='" + picture + "' class='foodpic' id = '"+ name +"'>");
-    var hyperlink = $("<p class='hyperlink'><a class= 'websiteurl' href ='" + website + "'>" + name + "</a></p><p> Ingredients: </p>");
-    $(recipecontain).append(foodpic);
-    $(recipecontain).append(hyperlink);
 
+  var getRecipeFunction = function() {
+    console.log("i'm in");
+    //get saved recipes from database
+    $.get("/api/recipes", function(data) {
+
+      console.log("saved recipes: " + data);
+      //for each of saved recipe
+      if (count < data.length) {
+
+        //find the name, website url, picture
+        var name = data[count].recipename;
+        var website = data[count].website;
+        var picture = data[count].picture;
+        hello = "#" + data[count].recipename.substring(0, 4);
+        console.log(hello);
+
+        //make a container
+        recipecontain = $("<div id = '" + data[count].recipename.substring(0, 4) + "'></div>");
+
+        //append name with website url, picture
+        var foodpic = $("<img src='" + picture + "' id ='" + name + "' class='foodpic' >");
+        var hyperlink = $("<p class='hyperlink'><a class= 'websiteurl' href ='" + website + "'>" + name + "</a></p><p> Ingredients: </p>");
+        $(recipecontain).append(foodpic);
+        $(recipecontain).append(hyperlink);
+
+        //append it to document
+        $(".card-action").append(recipecontain);
+
+        getIngredientsFunction(name);
+      }
+    });
+  };
+
+  var getIngredientsFunction = function(name) {
     //with that name, find all ingredients related to it
-    $.get("/api/ingredients/" + name, function(datas){
+    $.get("/api/ingredients/" + name, function(datas) {
 
-    //for each ingredient found in database,
-    for (var j = 0; j < datas.length; j++){
+      console.log(datas);
 
-      //make a container
-      var itemcontainer = $("<div class = 'itemcontain'></div>");
+      //for each ingredient found in database,
+      for (var j = 0; j < datas.length; j++) {
 
-      //append text of inredient, with the value of the ingredient and id of quantity
-      var item = $("<p value = '" + datas[j].ingredient + "' class = '" + datas[j].quantity + "'>" + datas[j].text + "</p>");
 
-      //add button that says add item to current list
-      var addbutt = $("<button class = 'additem'> + to list </button>");
-      $(itemcontainer).append(item);
-      $(itemcontainer).append(addbutt);
+        //make a container
+        var itemcontainer = $("<div class = 'itemcontain'></div>");
 
-      //append this container to the first container
-      $(recipecontain).append(itemcontainer);
+        //append text of inredient, with the value of the ingredient and id of quantity
+        var item = $("<p value = '" + datas[j].ingredient + "' class = '" + datas[j].quantity + "'>" + datas[j].text + "</p>");
+
+        //add button that says add item to current list
+        var addbutt = $("<button class = 'additem'> + to list </button>");
+        $(itemcontainer).append(item);
+        $(itemcontainer).append(addbutt);
+
+        //append this container to the first container
+
+        $(hello).append(itemcontainer);
       }
 
       //add a delete recipe button from saved lists
       var deletebutt = $("<button class = 'deleterecipe'> Delete recipe </button>");
-      $(recipecontain).append(deletebutt);
+      $(hello).append(deletebutt);
+      count++;
+      getRecipeFunction();
     });
+  };
 
-    //append it to the document
-    $(".card-action").append(recipecontain);
-  }
+  getRecipeFunction();
 
-});
 
-//on delete button
-  $(document).on('click', ".deleterecipe", function () {
+  //on delete button
+  $(document).on('click', ".deleterecipe", function() {
     console.log("just clicked delete");
     //grab the name of the recipe name
     var name = $(event.target).parent().find('img').attr("id");
     console.log("name to delete: " + name);
     //initiate the delete ajax call
     $.ajax({
-      method: "DELETE",
-      url: "/api/recipes/" + name
-    })
-    .done(function(){
-      console.log("just deleted a recipe from saved recipes");
-
-      //delete from ingredients
-      $.ajax({
         method: "DELETE",
-        url: "/api/ingredients/" + name
+        url: "/api/recipes/" + name
       })
-      .done(function(){
-        console.log("just deleted all ingredients of specific recipe");
+      .done(function() {
+        console.log("just deleted a recipe from saved recipes");
+
+        //delete from ingredients
+        $.ajax({
+            method: "DELETE",
+            url: "/api/ingredients/" + name
+          })
+          .done(function() {
+            console.log("just deleted all ingredients of specific recipe");
 
 
+          });
       });
-    });
 
     //hide this container
     console.log("hiding " + $(event.target).parent());
